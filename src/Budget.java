57afc448514c132efdafe787a5662ff2dbf20228
Alexandra <aborn601722@Alexandras-MacBook-Pro.local>
2017-07-27 11:13:07 -0400

    extracted methods to new Budget Class

--- src/Budget.java
+++ src/Budget.java
@@ -1,8 +1,52 @@
+import java.util.ArrayList;
+import java.util.Scanner;
+
 public class Budget {
 
-    public void VerifyBudget(){
-
+    public void printBudgetTable(ArrayList budgetCategories, double[] categoryAmount) {
+        for (int i = 0; i < budgetCategories.size(); i++) {
+            System.out.print((i + 1) + ". ");
+            System.out.print(budgetCategories.get(i) + "  ");
+            System.out.println(categoryAmount[i]);
+        }
     }
+
+    public void establishBudget(String namethis, double catAmount, ArrayList budgetCategories, double[] categoryAmount){
+        System.out.println("Enter the names of your budget categories here; press 'x' when finished.");
+        Scanner scan = new Scanner(System.in);
+
+        String nameThis = "x";
+        do {
+            System.out.println();
+            System.out.println("Enter a name for this category:");
+            nameThis = scan.nextLine();
+            budgetCategories.add(nameThis);
+            if (nameThis.equalsIgnoreCase("x")){
+                budgetCategories.remove(nameThis);
+            }
+        } while (!nameThis.equalsIgnoreCase("x"));
+
+        //double[] categoryAmount = new double[10];
+
+        for (int i = 0; i < budgetCategories.size(); i++) {
+            System.out.println();
+            System.out.println(budgetCategories.get(i));
+            System.out.println("Enter amount for this category: ");
+            catAmount = scan.nextDouble();
+            categoryAmount[i] = catAmount;
+        }
+    }
+
+    public void enterTransations(int toEdit, int deductAmount, double[] categoryAmount){
+        if (toEdit == 1) {
+            categoryAmount[toEdit - 1] = categoryAmount[toEdit - 1] - deductAmount;
+        } else if (toEdit == 2) {
+            categoryAmount[toEdit - 1] = categoryAmount[toEdit - 1] - deductAmount;
+        } else if (toEdit == 3) {
+            categoryAmount[toEdit - 1] = categoryAmount[toEdit - 1] - deductAmount;
+        }
+    }
+
 
 
 }
--- src/MainApp.java
+++ src/MainApp.java
@@ -3,7 +3,10 @@ import java.util.Scanner;
 
 public class MainApp {
 
+
     public static void main(String[] args) {
+
+        Budget budget = new Budget();
 
         Scanner scan = new Scanner(System.in);
 
@@ -15,58 +18,28 @@ public class MainApp {
 
         System.out.println();
 
-        ArrayList<String> budgetCategories = new ArrayList<String>();          //ESTABLISH BUDGET CATEGORY NAMES + AMOUNTS
-        System.out.println("Enter the names of your budget categories here; press 'x' when finished.");
-
+        ArrayList<String> budgetCategories = new ArrayList<String>();     //ESTABLISH BUDGET CATEGORY NAMES + AMOUNTS
         String nameThis = "x";
-        do {
-            System.out.println();
-            System.out.println("Enter a name for this category:");
-            nameThis = scan.nextLine();
-            budgetCategories.add(nameThis);
-            if (nameThis.equalsIgnoreCase("x")){
-                budgetCategories.remove(nameThis);
-            }
-        } while (!nameThis.equalsIgnoreCase("x"));
-
         double[] categoryAmount = new double[10];
-
-        for (int i = 0; i < budgetCategories.size(); i++) {
-            System.out.println();
-            System.out.println(budgetCategories.get(i));
-            System.out.println("Enter amount for this category: ");
-            double catAmount = scan.nextDouble();
-            categoryAmount[i] = catAmount;
-        }                                                               //ESTABLISH BUDGET CATEGORY NAMES + AMOUNTS
+        double catAmount = 0;
+        budget.establishBudget(nameThis, catAmount, budgetCategories, categoryAmount);  //ESTABLISH BUDGET CATEGORY NAMES + AMOUNTS
 
         System.out.println();
 
-        System.out.println("Here's your monthly budget:");              //PRINT MONTHLY BUDGET TABLE
+        System.out.println("Here's your monthly budget:");
 
         System.out.println();
 
-        for (int i = 0; i < budgetCategories.size(); i++) {
-            System.out.print((i + 1) + ". ");
-            System.out.print(budgetCategories.get(i) + "  ");
-            System.out.println(categoryAmount[i]);
-
-        }                                                               //PRINT MONTHLY BUDGET TABLE
+        budget.printBudgetTable(budgetCategories, categoryAmount);   //PRINT MONTHLY BUDGET TABLE
 
         System.out.println();
-        scan.nextLine();
 
         System.out.println("Enter a category number to save a transaction: ");      //ENTER EXPENSES PER CATEGORY
         int toEdit = scan.nextInt();
         System.out.println("Enter amount spent in this category: ");
         int deductAmount = scan.nextInt();
 
-        if (toEdit == 1) {
-            categoryAmount[toEdit - 1] = categoryAmount[toEdit - 1] - deductAmount;
-        } else if (toEdit == 2) {
-            categoryAmount[toEdit - 1] = categoryAmount[toEdit - 1] - deductAmount;
-        } else if (toEdit == 3) {
-            categoryAmount[toEdit - 1] = categoryAmount[toEdit - 1] - deductAmount;
-        }                                                                           //ENTER EXPENSES PER CATEGORY
+        budget.enterTransations(toEdit, deductAmount, categoryAmount);             //ENTER EXPENSES PER CATEGORY
 
         System.out.println();
 
@@ -74,12 +47,8 @@ public class MainApp {
 
         System.out.println();
 
-        for (int i = 0; i < budgetCategories.size(); i++) {
-            System.out.print((i + 1) + ". ");
-            System.out.print(budgetCategories.get(i) + "  ");
-            System.out.println(categoryAmount[i]);
+        budget.printBudgetTable(budgetCategories, categoryAmount);          //PRINT MONTHLY BUDGET TABLE
 
-        }
 
 
     }
